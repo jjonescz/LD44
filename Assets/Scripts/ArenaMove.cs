c9ad924bac34fa44d54d4868df40045bb583ef76
Jan Joneš <jjones@outlook.cz>
2019-04-27 12:17:11 +0200

    Backup

---
+++ Assets/Scripts/ArenaMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("X");
+        float y = Input.GetAxis("Y");
+    }
+}
